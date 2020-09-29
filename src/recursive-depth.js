433633adb3728504c9e60597d237ec404ab28763
Karina Koldaeva <71209378+KarinaKold@users.noreply.github.com>
2020-09-29 22:22:30 +0300

    feat: solve recursive

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,13 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(arr) {
-      let count = 0;
-      if (Array.isArray(arr)) {
-          arr.forEach(element => count = Math.max(count, this.calculateDepth(element)));
-              return ++count;
-      } else {
-          return 0;
-      }
-  }
+    calculateDepth(arr) {
+        if (arr.length === 0) return 1;
+        if (!Array.isArray(arr)) return 0;
+        return 1 + Math.max(...arr.map(x => this.calculateDepth(x)));
+    }
 };
