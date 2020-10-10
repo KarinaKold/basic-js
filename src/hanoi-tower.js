12e4a240fc1c623e8495a81b7530d94513a6a904
Karina Koldaeva <71209378+KarinaKold@users.noreply.github.com>
2020-10-10 23:24:49 +0300

    feat: solve hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,11 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-    var result = {
-        turns: 0,
-        seconds: 0
-    };
-    result["turns"] = Math.pow(2, disksNumber) - 1;
-    result["seconds"] = (result.turns / (turnsSpeed/3600));
-    return result;
+    let turns = 2**disksNumber - 1;
+    let seconds = Math.floor(turns * 3600 / turnsSpeed);
+    return {turns, seconds};
 };
